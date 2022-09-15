774abe0e203314408ac241a91ce48e1ec3f5c5d4
Tourneux Maxence <tourneuxmaxence@gmail.com>
2022-09-15 14:27:51 +0200

    OC-1: directives ngIf, ngFor

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,43 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FaceSnap } from './models/face-snap.models';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'snapface';
+export class AppComponent implements OnInit {
+  faceSnaps!: FaceSnap[];
+  mySnap!: FaceSnap;
+  myOtherSnap!: FaceSnap;
+  myLastSnap!: FaceSnap;
+
+  ngOnInit() {
+    this.faceSnaps = [
+      {
+        title: 'Archibald',
+        description: 'Mon meilleur ami depuis toujours',
+        imageUrl: 'https://images.unsplash.com/photo-1567169866456-a0759b6bb0c8',
+        createdDate: new Date(),
+        snaps: 0,
+        location: 'Paris'
+      },
+      {
+        title: 'Three Rock Mountains',
+        description: 'Un endroit magnifique pour les randonnées',
+        imageUrl: 'https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Three_Rock_Mountain_Southern_Tor.jpg/1920px-Three_Rock_Mountain_Southern_Tor.jpg',
+        createdDate: new Date(),
+        snaps: 0,
+        location: 'Wicklow'
+      },
+      {
+        title: 'Un bon repas',
+        description: 'Un plat savoureux',
+        imageUrl: 'https://n6g3q5q8.rocketcdn.me/wp-content/uploads/2022/08/Tajine-aux-crevettes-au-Thermomix-Un-delicieux-plat-marocain-a-preparer-470x264.jpg',
+        createdDate: new Date(),
+        snaps: 0
+      }
+    ];
+  }
+
 }
---
+++ src/app/face-snap/models/face-snap.models.ts
@@ -0,0 +1,12 @@
+export class FaceSnap {
+
+  constructor(
+    public title: string, 
+    public descritpion: string,
+    public imageUrl: string,
+    public createdDate: Date,
+    public snaps: number) {
+  }
+
+
+}
